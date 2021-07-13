cb245b1bb2d4afa519ae138d0256e154c3da6e66
S.MUKILAN <65155327+MUKILAN-2003@users.noreply.github.com>
2021-07-13 20:31:36 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,17 @@
 <?php
-/*Connect With The Local db*/
+
+
+/*Connect With The Local db
+Coloums are,
+1.id
+2.Name
+3.Title
+4.Ingredient
+5.Procedure
+6.CreateAt
+*/
+
+
 $conn = mysqli_connect('localhost', 'muki', 'php_db', 'Recipe_Store');
 if (!$conn) {
     echo 'Connection Error  ' . mysqli_connect_error();
