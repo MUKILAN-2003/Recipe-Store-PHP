573e42d167af958df83d26a0c0e8c5697b995c61
S.MUKILAN <65155327+MUKILAN-2003@users.noreply.github.com>
2021-07-13 20:51:11 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -3,12 +3,12 @@
 
 /*Connect With The Local db
 Coloums are,
-1.id
-2.Name
-3.Title
-4.Ingredient
-5.Procedure
-6.CreateAt
+1.id(A.I)
+2.name(255 Char Length)
+3.title(255 Char Length)
+4.ingredient(255 Char Length)
+5.recipe_procedure(1280 Char Length)
+6.create_at(TimeStamp)
 */
 
 
